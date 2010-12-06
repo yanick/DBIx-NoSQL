404cf13f2a5f2bdcfe19c1032b75df4a89b8725a
Robert Krimen <robertkrimen@gmail.com>
2010-12-06 00:33:09 -0500

    import DBIx-NoSQL 0.0015
    git-cpan-module:   DBIx-NoSQL
    git-cpan-version:  0.0015
    git-cpan-authorid: ROKR

--- lib/DBIx/NoSQL.pm
+++ lib/DBIx/NoSQL.pm
@@ -1,6 +1,6 @@
 package DBIx::NoSQL;
 BEGIN {
-  $DBIx::NoSQL::VERSION = '0.0014';
+  $DBIx::NoSQL::VERSION = '0.0015';
 }
 # ABSTRACT: NoSQL-ish overlay for an SQL database
 
@@ -31,7 +31,7 @@ DBIx::NoSQL - NoSQL-ish overlay for an SQL database
 
 =head1 VERSION
 
-version 0.0014
+version 0.0015
 
 =head1 SYNOPSIS
 
--- lib/DBIx/NoSQLite.pm
+++ lib/DBIx/NoSQLite.pm
@@ -1,6 +1,6 @@
 package DBIx::NoSQLite;
 BEGIN {
-  $DBIx::NoSQLite::VERSION = '0.0014';
+  $DBIx::NoSQLite::VERSION = '0.0015';
 }
 # ABSTRACT: An embedded, NoSQL SQLite database with SQL indexing
 
@@ -33,7 +33,7 @@ DBIx::NoSQLite - An embedded, NoSQL SQLite database with SQL indexing
 
 =head1 VERSION
 
-version 0.0014
+version 0.0015
 
 =head1 SYNOPSIS
 
